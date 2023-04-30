b06c583e75c61195bffc71126e96ec402a94c36c
OUHALIMA SOFIANE <s.ouhalima@bneder.dz>
2023-04-30 15:06:54 +0200

    PortFolio

--- mail/contact.js
+++ mail/contact.js
@@ -12,7 +12,7 @@ $(function () {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true);
       $.ajax({
-        url: "contact.php",
+        url: "mail/contact.php",
         type: "POST",
         data: {
           name: name,
@@ -21,18 +21,20 @@ $(function () {
           message: message,
         },
         cache: false,
-        success: function () {
-          $("#success").html("<div class='alert alert-success'>");
-          $("#success > .alert-success")
-            .html(
-              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-            )
-            .append("</button>");
-          $("#success > .alert-success").append(
-            "<strong>Your message has been sent. </strong>"
-          );
-          $("#success > .alert-success").append("</div>");
-          $("#contactForm").trigger("reset");
+        success: function (e) {
+          var data = JSON.parse(e);
+          console.log(data);
+          // $("#success").html("<div class='alert alert-success'>");
+          // $("#success > .alert-success")
+          //   .html(
+          //     "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+          //   )
+          //   .append("</button>");
+          // $("#success > .alert-success").append(
+          //   "<strong>Your message has been sent. </strong>"
+          // );
+          // $("#success > .alert-success").append("</div>");
+          // $("#contactForm").trigger("reset");
         },
         error: function () {
           $("#success").html("<div class='alert alert-danger'>");
--- mail/contact.php
+++ mail/contact.php
@@ -1,24 +1,18 @@
 <?php
-// if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message'])) {
-//   http_response_code(500);
-//   exit();
-// }
-
-
-// $to = "sofianeou93@gmail.com"; // Change this email to your own email address
-// $subject = "$m_subject: $name";
-// $body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\nEmail: $email\nSubject: $m_subject\nMessage:\n$message";
-// $headers = "From: $email\r\nReply-To: $email\r\n";
-
-// if (!mail($to, $subject, $body, $headers)) {
-//   http_response_code(500);
-// }
+if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || empty($_POST['mail'])) {
+  http_response_code(500);
+  exit();
+}
+$to = "sofianeou93@gmail.com"; // Change this email to your own email address
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = (($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-$headers = 'From: sofianeou93@gmail.com'       . "\r\n" .
-  'Reply-To: sofianeou93@gmail.com' . "\r\n" .
-  'X-Mailer: PHP/' . phpversion();
+$body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
 
-mail($to, $subject, $message, $headers);
+$headers = 'From: "' . $email . '"'       . "\r\n" .
+  'Reply-To: "' . $email . '"' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+if (!mail($to, $subject, $message, $headers)) {
+  http_response_code(500);
+}
